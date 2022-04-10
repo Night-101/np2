d6414dacb715ea7fb6c255eb111a2aabdb9d297a
Night101 <91742459+notaweed@users.noreply.github.com>
2022-04-10 16:00:59 +0530

    Update login.dart

--- login.dart
+++ login.dart
@@ -6,18 +6,19 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
       resizeToAvoidBottomInset: false,
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xff2c2f33),
       appBar: AppBar(
         elevation: 0,
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xff2c2f33),
         leading: IconButton(
           onPressed: () {
             Navigator.pop(context);
           },
           icon: Icon(Icons.arrow_back_ios,
             size: 20,
-            color: Colors.black,),
+            color: Colors.white70,),
         ),
       ),
       body: Container(
@@ -32,12 +33,12 @@ class LoginPage extends StatelessWidget {
                 Column(
                   children: <Widget>[
                     Text("Login",
-                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),
+                      style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),),
                     SizedBox(height: 20,),
                     Text("Login to your account",
                       style: TextStyle(
                           fontSize: 15,
-                          color:Colors.grey[700]),)
+                          color:Colors.white70),)
                   ],
                 ),
                 Padding(
@@ -59,7 +60,7 @@ class LoginPage extends StatelessWidget {
                       onPressed: () {
                         Navigator.push(context, MaterialPageRoute(builder: (context) => List()));
                       },
-                      color: Color(0xff0095FF),
+                      color: Color(0xff7289da),
                       elevation: 0,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(50),
@@ -77,7 +78,7 @@ class LoginPage extends StatelessWidget {
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Text("Don't have an account?"),
+                    Text("Don't have an account?", style: TextStyle(color: Colors.white),),
                     GestureDetector(
                       onTap: () {
                         Navigator.push(context, MaterialPageRoute(builder: (context) => SignupPage()));
@@ -85,6 +86,7 @@ class LoginPage extends StatelessWidget {
                     child: Text(" Sign up", style: TextStyle(
                       fontWeight: FontWeight.w600,
                       fontSize: 18,
+                      color: Colors.white
 
                     ),)
                     ),
@@ -108,7 +110,7 @@ Widget inputFile({label, obscureText = false})
         style: TextStyle(
             fontSize: 15,
             fontWeight: FontWeight.w400,
-            color:Colors.black87
+            color:Colors.white
         ),
       ),
       SizedBox(
