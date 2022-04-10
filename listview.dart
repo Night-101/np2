af91eda2289dd32e86bc3b544a74691ae8b2f71d
Night101 <91742459+notaweed@users.noreply.github.com>
2022-04-10 16:00:28 +0530

    Update listview.dart

--- listview.dart
+++ listview.dart
@@ -4,22 +4,24 @@ class List extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color(0xff2c2f33),
         appBar: AppBar(
           elevation: 0,
-          backgroundColor: Colors.white,
+          backgroundColor: Color(0xff2c2f33),
           leading: IconButton(
             onPressed: () {
               Navigator.pop(context);
             },
             icon: Icon(Icons.arrow_back_ios,
               size: 20,
-              color: Colors.black,),
+              color: Colors.white70),
           ),
         ),
       body: ListView(
         children: [
           Container(
             child: ListTile(
+              textColor: Colors.white,
               title: Text("text1"),
               subtitle: Text("sub"),
               onTap: (){},
@@ -27,6 +29,7 @@ class List extends StatelessWidget {
           ),
           Container(
             child: ListTile(
+              textColor: Colors.white,
               title: Text("text2"),
               subtitle: Text("sub"),
               onTap: (){},
@@ -34,6 +37,7 @@ class List extends StatelessWidget {
           ),
           Container(
             child: ListTile(
+              textColor: Colors.white,
               title: Text("text3"),
               subtitle: Text("sub"),
               onTap: (){},
